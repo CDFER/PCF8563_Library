ea3237407793f22b4afbfe624dd2855a21155cc8
CDFER <cdirksfer@gamil.com>
2023-06-10 14:22:39 +1200

    add comments and tidy up code

--- pcf8563.cpp
+++ pcf8563.cpp
@@ -32,231 +32,231 @@ github:https://github.com/lewisxhe/PCF8563_Library
 #include <sys/time.h>
 #include <time.h>
 
-int PCF8563_Class::begin(TwoWire &port, uint8_t addr) {
-	_i2cPort = &port;
-	_address = addr;
-	_i2cPort->beginTransmission(_address);
-	return (_i2cPort->endTransmission() == 0);
+uint8_t PCF8563_Class::begin(TwoWire &port, int addr) {
+	_i2cPort = &port;						// Set the I2C port to be used for communication
+	_address = addr;						// Set the I2C address of the PCF8563 chip
+	_i2cPort->beginTransmission(_address);	// Begin the I2C transmission to the PCF8563 chip
+	return _i2cPort->endTransmission();		// Check if the transmission was successful and return the result
 }
 
 void PCF8563_Class::setDateTime(RTC_Date date) {
 	setDateTime(date.year, date.month, date.day, date.hour, date.minute, date.second);
+	// Call the setDateTime function with individual date and time components
 }
 
 uint32_t PCF8563_Class::getDayOfWeek(uint32_t day, uint32_t month, uint32_t year) {
 	uint32_t val;
-	if (month < 3) {
-		month = 12u + month;
-		year--;
+	if (month < 3) {		  // Check if the month is January or February
+		month = 12u + month;  // Adjust the month value accordingly
+		year--;				  // Decrease the year value by 1
 	}
 	val = (day + (((month + 1u) * 26u) / 10u) + year + (year / 4u) + (6u * (year / 100u)) + (year / 400u)) % 7u;
+	// Calculate the day of the week using Zeller's Congruence formula
 	if (0u == val) {
-		val = 7;
+		val = 7;  // Adjust Sunday's value to 7 instead of 0
 	}
-	return (val - 1);
+	return (val - 1);  // Subtract 1 to match the range 0-6 for days of the week
 }
 
-void PCF8563_Class::setDateTime(uint16_t year,
-								uint8_t month,
-								uint8_t day,
-								uint8_t hour,
-								uint8_t minute,
-								uint8_t second) {
-	_data[0] = _dec_to_bcd(second) & (~PCF8563_VOL_LOW_MASK);
-	_data[1] = _dec_to_bcd(minute);
-	_data[2] = _dec_to_bcd(hour);
-	_data[3] = _dec_to_bcd(day);
-	_data[4] = getDayOfWeek(day, month, year);
-	_data[5] = _dec_to_bcd(month);
-	_data[6] = _dec_to_bcd(year % 100);
+void PCF8563_Class::setDateTime(uint16_t year, uint8_t month, uint8_t day, uint8_t hour, uint8_t minute, uint8_t second) {
+	_data[0] = _dec_to_bcd(second) & (~PCF8563_VOL_LOW_MASK);  // Convert seconds to BCD and store in data array
+	_data[1] = _dec_to_bcd(minute);							   // Convert minutes to BCD and store in data array
+	_data[2] = _dec_to_bcd(hour);							   // Convert hours to BCD and store in data array
+	_data[3] = _dec_to_bcd(day);							   // Convert day to BCD and store in data array
+	_data[4] = getDayOfWeek(day, month, year);				   // Get the day of the week and store in data array
+	_data[5] = _dec_to_bcd(month);							   // Convert month to BCD and store in data array
+	_data[6] = _dec_to_bcd(year % 100);						   // Convert last two digits of the year to BCD and store in data array
 
 	if ((2000 % year) == 2000) {
-		_data[5] &= (~PCF8563_CENTURY_MASK);
+		_data[5] &= (~PCF8563_CENTURY_MASK);  // Clear the century bit if the year is 2000
 	} else {
-		_data[5] |= PCF8563_CENTURY_MASK;
+		_data[5] |= PCF8563_CENTURY_MASK;  // Set the century bit if the year is not 2000
 	}
-	_writeByte(PCF8563_SEC_REG, 7, _data);
+	_writeByte(PCF8563_SEC_REG, 7, _data);	// Write the date and time values to the PCF8563 chip
 }
 
 bool PCF8563_Class::isValid() {
-	_readByte(PCF8563_SEC_REG, 1, &_isValid);
-	return !(_isValid & (1 << 7));
+	_readByte(PCF8563_SEC_REG, 1, &_isValid);  // Read the seconds register to check validity
+	return !(_isValid & (1 << 7));			   // Check the validity flag and return the result
 }
-
 RTC_Date PCF8563_Class::getDateTime() {
-	uint16_t year;
-	uint8_t century = 0;
-	_readByte(PCF8563_SEC_REG, 7, _data);
-	_voltageLow = (_data[0] & PCF8563_VOL_LOW_MASK);
-	_data[0] = _bcd_to_dec(_data[0] & (~PCF8563_VOL_LOW_MASK));
-	_data[1] = _bcd_to_dec(_data[1] & PCF8563_minuteS_MASK);
-	_data[2] = _bcd_to_dec(_data[2] & PCF8563_HOUR_MASK);
-	_data[3] = _bcd_to_dec(_data[3] & PCF8563_DAY_MASK);
-	_data[4] = _bcd_to_dec(_data[4] & PCF8563_WEEKDAY_MASK);
-	century = _data[5] & PCF8563_CENTURY_MASK;
-	_data[5] = _bcd_to_dec(_data[5] & PCF8563_MONTH_MASK);
-	year = _bcd_to_dec(_data[6]);
+	uint16_t year;												 // Variable to store the year
+	uint8_t century = 0;										 // Variable to store the century flag
+	_readByte(PCF8563_SEC_REG, 7, _data);						 // Read the date and time registers
+	_voltageLow = (_data[0] & PCF8563_VOL_LOW_MASK);			 // Check if voltage low flag is set
+	_data[0] = _bcd_to_dec(_data[0] & (~PCF8563_VOL_LOW_MASK));	 // Convert seconds from BCD to decimal
+	_data[1] = _bcd_to_dec(_data[1] & PCF8563_minuteS_MASK);	 // Convert minutes from BCD to decimal
+	_data[2] = _bcd_to_dec(_data[2] & PCF8563_HOUR_MASK);		 // Convert hours from BCD to decimal
+	_data[3] = _bcd_to_dec(_data[3] & PCF8563_DAY_MASK);		 // Convert day from BCD to decimal
+	_data[4] = _bcd_to_dec(_data[4] & PCF8563_WEEKDAY_MASK);	 // Convert weekday from BCD to decimal
+	century = _data[5] & PCF8563_CENTURY_MASK;					 // Check the century flag
+	_data[5] = _bcd_to_dec(_data[5] & PCF8563_MONTH_MASK);		 // Convert month from BCD to decimal
+	year = _bcd_to_dec(_data[6]);								 // Convert year from BCD to decimal
 	// century :  0 = 1900 , 1 = 2000
-	year = century ? 1900 + year : 2000 + year;
+	year = century ? 1900 + year : 2000 + year;	 // Determine the actual year based on the century flag
 	return RTC_Date(
 		year,
 		_data[5],
 		_data[3],
 		_data[2],
 		_data[1],
-		_data[0]);
+		_data[0]);	// Create and return RTC_Date object with the obtained date and time values
 }
 
 RTC_Alarm PCF8563_Class::getAlarm() {
-	_readByte(PCF8563_ALRM_MIN_REG, 4, _data);
-	_data[0] = _bcd_to_dec(_data[0] & (~PCF8563_minuteS_MASK));
-	_data[1] = _bcd_to_dec(_data[1] & (~PCF8563_HOUR_MASK));
-	_data[2] = _bcd_to_dec(_data[2] & (~PCF8563_DAY_MASK));
-	_data[3] = _bcd_to_dec(_data[3] & (~PCF8563_WEEKDAY_MASK));
-	return RTC_Alarm(_data[0], _data[1], _data[2], _data[3]);
+	_readByte(PCF8563_ALRM_MIN_REG, 4, _data);					 // Read the alarm registers
+	_data[0] = _bcd_to_dec(_data[0] & (~PCF8563_minuteS_MASK));	 // Convert minutes from BCD to decimal
+	_data[1] = _bcd_to_dec(_data[1] & (~PCF8563_HOUR_MASK));	 // Convert hours from BCD to decimal
+	_data[2] = _bcd_to_dec(_data[2] & (~PCF8563_DAY_MASK));		 // Convert day from BCD to decimal
+	_data[3] = _bcd_to_dec(_data[3] & (~PCF8563_WEEKDAY_MASK));	 // Convert weekday from BCD to decimal
+	return RTC_Alarm(_data[0], _data[1], _data[2], _data[3]);	 // Create and return RTC_Alarm object with the obtained alarm values
 }
 
 void PCF8563_Class::enableAlarm() {
-	_readByte(PCF8563_STAT2_REG, 1, _data);
-	_data[0] &= ~PCF8563_ALARM_AF;
-	_data[0] |= (PCF8563_TIMER_TF | PCF8563_ALARM_AIE);
-	_writeByte(PCF8563_STAT2_REG, 1, _data);
+	_readByte(PCF8563_STAT2_REG, 1, _data);				 // Read the status register 2
+	_data[0] &= ~PCF8563_ALARM_AF;						 // Clear the alarm flag
+	_data[0] |= (PCF8563_TIMER_TF | PCF8563_ALARM_AIE);	 // Set the alarm enable and timer flag
+	_writeByte(PCF8563_STAT2_REG, 1, _data);			 // Write back the updated status register 2
 }
 
 void PCF8563_Class::disableAlarm() {
-	_readByte(PCF8563_STAT2_REG, 1, _data);
-	_data[0] &= ~(PCF8563_ALARM_AF | PCF8563_ALARM_AIE);
-	_data[0] |= PCF8563_TIMER_TF;
-	_writeByte(PCF8563_STAT2_REG, 1, _data);
+	_readByte(PCF8563_STAT2_REG, 1, _data);				  // Read the status register 2
+	_data[0] &= ~(PCF8563_ALARM_AF | PCF8563_ALARM_AIE);  // Clear the alarm flag and alarm enable
+	_data[0] |= PCF8563_TIMER_TF;						  // Set the timer flag
+	_writeByte(PCF8563_STAT2_REG, 1, _data);			  // Write back the updated status register 2
 }
 
 void PCF8563_Class::resetAlarm() {
-	_readByte(PCF8563_STAT2_REG, 1, _data);
-	_data[0] &= ~(PCF8563_ALARM_AF);
-	_data[0] |= PCF8563_TIMER_TF;
-	_writeByte(PCF8563_STAT2_REG, 1, _data);
+	_readByte(PCF8563_STAT2_REG, 1, _data);	  // Read the status register 2
+	_data[0] &= ~(PCF8563_ALARM_AF);		  // Clear the alarm flag
+	_data[0] |= PCF8563_TIMER_TF;			  // Set the timer flag
+	_writeByte(PCF8563_STAT2_REG, 1, _data);  // Write back the updated status register 2
 }
-
 bool PCF8563_Class::alarmActive() {
-	_readByte(PCF8563_STAT2_REG, 1, _data);
-	return (bool)(_data[0] & PCF8563_ALARM_AF);
+	_readByte(PCF8563_STAT2_REG, 1, _data);		 // Read the status register 2
+	return (bool)(_data[0] & PCF8563_ALARM_AF);	 // Check if the alarm flag is set
 }
 
 void PCF8563_Class::setAlarm(RTC_Alarm alarm) {
-	setAlarm(alarm.minute, alarm.hour, alarm.day, alarm.weekday);
+	setAlarm(alarm.minute, alarm.hour, alarm.day, alarm.weekday);  // Call the setAlarm function with individual alarm parameters
 }
 
 void PCF8563_Class::setAlarm(uint8_t hour, uint8_t minute, uint8_t day, uint8_t weekday) {
 	if (minute != PCF8563_NO_ALARM) {
-		_data[0] = _dec_to_bcd(constrain(minute, 0, 59));
-		_data[0] &= ~PCF8563_ALARM_ENABLE;
+		_data[0] = _dec_to_bcd(constrain(minute, 0, 59));  // Convert minutes to BCD and store in data[0]
+		_data[0] &= ~PCF8563_ALARM_ENABLE;				   // Clear the alarm enable bit
 	} else {
-		_data[0] = PCF8563_ALARM_ENABLE;
+		_data[0] = PCF8563_ALARM_ENABLE;  // Set the alarm enable bit
 	}
 
 	if (hour != PCF8563_NO_ALARM) {
-		_data[1] = _dec_to_bcd(constrain(hour, 0, 23));
-		_data[1] &= ~PCF8563_ALARM_ENABLE;
+		_data[1] = _dec_to_bcd(constrain(hour, 0, 23));	 // Convert hours to BCD and store in data[1]
+		_data[1] &= ~PCF8563_ALARM_ENABLE;				 // Clear the alarm enable bit
 	} else {
-		_data[1] = PCF8563_ALARM_ENABLE;
+		_data[1] = PCF8563_ALARM_ENABLE;  // Set the alarm enable bit
 	}
+
 	if (day != PCF8563_NO_ALARM) {
-		_data[2] = _dec_to_bcd(constrain(day, 1, 31));
-		_data[2] &= ~PCF8563_ALARM_ENABLE;
+		_data[2] = _dec_to_bcd(constrain(day, 1, 31));	// Convert day to BCD and store in data[2]
+		_data[2] &= ~PCF8563_ALARM_ENABLE;				// Clear the alarm enable bit
 	} else {
-		_data[2] = PCF8563_ALARM_ENABLE;
+		_data[2] = PCF8563_ALARM_ENABLE;  // Set the alarm enable bit
 	}
+
 	if (weekday != PCF8563_NO_ALARM) {
-		_data[3] = _dec_to_bcd(constrain(weekday, 0, 6));
-		_data[3] &= ~PCF8563_ALARM_ENABLE;
+		_data[3] = _dec_to_bcd(constrain(weekday, 0, 6));  // Convert weekday to BCD and store in data[3]
+		_data[3] &= ~PCF8563_ALARM_ENABLE;				   // Clear the alarm enable bit
 	} else {
-		_data[3] = PCF8563_ALARM_ENABLE;
+		_data[3] = PCF8563_ALARM_ENABLE;  // Set the alarm enable bit
 	}
-	_writeByte(PCF8563_ALRM_MIN_REG, 4, _data);
+
+	_writeByte(PCF8563_ALRM_MIN_REG, 4, _data);	 // Write the alarm data to the alarm registers
 }
 
 void PCF8563_Class::setAlarmByMinutes(uint8_t minute) {
-	setAlarm(PCF8563_NO_ALARM, minute, PCF8563_NO_ALARM, PCF8563_NO_ALARM);
+	setAlarm(PCF8563_NO_ALARM, minute, PCF8563_NO_ALARM, PCF8563_NO_ALARM);	 // Set the alarm only for minutes
 }
+
 void PCF8563_Class::setAlarmByDays(uint8_t day) {
-	setAlarm(PCF8563_NO_ALARM, PCF8563_NO_ALARM, day, PCF8563_NO_ALARM);
+	setAlarm(PCF8563_NO_ALARM, PCF8563_NO_ALARM, day, PCF8563_NO_ALARM);  // Set the alarm only for days
 }
+
 void PCF8563_Class::setAlarmByHours(uint8_t hour) {
-	setAlarm(hour, PCF8563_NO_ALARM, PCF8563_NO_ALARM, PCF8563_NO_ALARM);
+	setAlarm(hour, PCF8563_NO_ALARM, PCF8563_NO_ALARM, PCF8563_NO_ALARM);  // Set the alarm only for hours
 }
+
 void PCF8563_Class::setAlarmByWeekDay(uint8_t weekday) {
-	setAlarm(PCF8563_NO_ALARM, PCF8563_NO_ALARM, PCF8563_NO_ALARM, weekday);
+	setAlarm(PCF8563_NO_ALARM, PCF8563_NO_ALARM, PCF8563_NO_ALARM, weekday);  // Set the alarm only for weekdays
 }
 
 bool PCF8563_Class::isTimerEnable() {
-	_readByte(PCF8563_STAT2_REG, 1, &_data[0]);
-	_readByte(PCF8563_TIMER1_REG, 1, &_data[1]);
-	if (_data[0] & PCF8563_TIMER_TIE) {
-		return _data[1] & PCF8563_TIMER_TE ? true : false;
+	_readByte(PCF8563_STAT2_REG, 1, &_data[0]);				// Read the status register 2
+	_readByte(PCF8563_TIMER1_REG, 1, &_data[1]);			// Read the timer register
+	if (_data[0] & PCF8563_TIMER_TIE) {						// Check if timer interrupt is enabled
+		return _data[1] & PCF8563_TIMER_TE ? true : false;	// Check if timer is enabled
 	}
-	return false;
+	return false;  // Timer is not enabled
 }
-
 bool PCF8563_Class::isTimerActive() {
-	_readByte(PCF8563_STAT2_REG, 1, _data);
-	return (bool)_data[0] & PCF8563_TIMER_TF;
+	_readByte(PCF8563_STAT2_REG, 1, _data);	   // Read the status register 2
+	return (bool)_data[0] & PCF8563_TIMER_TF;  // Check if the timer flag is set
 }
 
 void PCF8563_Class::enableTimer() {
-	_readByte(PCF8563_STAT2_REG, 1, &_data[0]);
-	_readByte(PCF8563_TIMER1_REG, 1, &_data[1]);
-	_data[0] &= ~PCF8563_TIMER_TF;
-	_data[0] |= (PCF8563_ALARM_AF | PCF8563_TIMER_TIE);
-	_data[1] |= PCF8563_TIMER_TE;
-	_writeByte(PCF8563_STAT2_REG, 1, &_data[0]);
-	_writeByte(PCF8563_TIMER1_REG, 1, &_data[1]);
+	_readByte(PCF8563_STAT2_REG, 1, &_data[0]);			 // Read the status register 2
+	_readByte(PCF8563_TIMER1_REG, 1, &_data[1]);		 // Read the timer register
+	_data[0] &= ~PCF8563_TIMER_TF;						 // Clear the timer flag
+	_data[0] |= (PCF8563_ALARM_AF | PCF8563_TIMER_TIE);	 // Enable alarm interrupt and timer interrupt
+	_data[1] |= PCF8563_TIMER_TE;						 // Enable the timer
+	_writeByte(PCF8563_STAT2_REG, 1, &_data[0]);		 // Write the updated status register 2
+	_writeByte(PCF8563_TIMER1_REG, 1, &_data[1]);		 // Write the updated timer register
 }
 
 void PCF8563_Class::disableTimer() {
-	_readByte(PCF8563_STAT2_REG, 1, _data);
-	_data[0] &= ~PCF8563_TIMER_TF;
-	_data[1] |= PCF8563_ALARM_AF;
-	_writeByte(PCF8563_STAT2_REG, 1, _data);
+	_readByte(PCF8563_STAT2_REG, 1, _data);	  // Read the status register 2
+	_data[0] &= ~PCF8563_TIMER_TF;			  // Clear the timer flag
+	_data[1] |= PCF8563_ALARM_AF;			  // Set the alarm flag
+	_writeByte(PCF8563_STAT2_REG, 1, _data);  // Write the updated status register 2
 }
 
 void PCF8563_Class::setTimer(uint8_t val, uint8_t freq, bool enInterrupt) {
-	_readByte(PCF8563_STAT2_REG, 1, &_data[0]);
-	_readByte(PCF8563_TIMER1_REG, 1, &_data[1]);
+	_readByte(PCF8563_STAT2_REG, 1, &_data[0]);	  // Read the status register 2
+	_readByte(PCF8563_TIMER1_REG, 1, &_data[1]);  // Read the timer register
 	if (enInterrupt) {
-		_data[0] |= 1 << 4;
+		_data[0] |= 1 << 4;	 // Set the interrupt enable bit
 	} else {
-		_data[0] &= ~(1 << 4);
+		_data[0] &= ~(1 << 4);	// Clear the interrupt enable bit
 	}
-	_data[1] |= (freq & PCF8563_TIMER_TD10);
-	_data[2] = val;
-	_writeByte(PCF8563_STAT2_REG, 1, &_data[0]);
-	_writeByte(PCF8563_TIMER1_REG, 1, &_data[1]);
-	_writeByte(PCF8563_TIMER2_REG, 1, &_data[2]);
+	_data[1] |= (freq & PCF8563_TIMER_TD10);	   // Set the timer frequency
+	_data[2] = val;								   // Set the timer value
+	_writeByte(PCF8563_STAT2_REG, 1, &_data[0]);   // Write the updated status register 2
+	_writeByte(PCF8563_TIMER1_REG, 1, &_data[1]);  // Write the updated timer register
+	_writeByte(PCF8563_TIMER2_REG, 1, &_data[2]);  // Write the timer value
 }
 
 void PCF8563_Class::clearTimer() {
-	_readByte(PCF8563_STAT2_REG, 1, _data);
-	_data[0] &= ~(PCF8563_TIMER_TF | PCF8563_TIMER_TIE);
-	_data[0] |= PCF8563_ALARM_AF;
-	_data[1] = 0x00;
-	_writeByte(PCF8563_STAT2_REG, 1, &_data[0]);
-	_writeByte(PCF8563_TIMER1_REG, 1, &_data[1]);
+	_readByte(PCF8563_STAT2_REG, 1, _data);				  // Read the status register 2
+	_data[0] &= ~(PCF8563_TIMER_TF | PCF8563_TIMER_TIE);  // Clear the timer flag and timer interrupt enable bits
+	_data[0] |= PCF8563_ALARM_AF;						  // Set the alarm flag
+	_data[1] = 0x00;									  // Clear the timer value
+	_writeByte(PCF8563_STAT2_REG, 1, &_data[0]);		  // Write the updated status register 2
+	_writeByte(PCF8563_TIMER1_REG, 1, &_data[1]);		  // Write the updated timer register
 }
 
 bool PCF8563_Class::enableCLK(uint8_t freq) {
-	if (freq >= PCF8563_CLK_MAX) return false;
-	_data[0] = freq | PCF8563_CLK_ENABLE;
-	_writeByte(PCF8563_SQW_REG, 1, _data);
-	return true;
+	if (freq >= PCF8563_CLK_MAX) return false;	// Check if the frequency is valid
+	_data[0] = freq | PCF8563_CLK_ENABLE;		// Set the frequency and enable the clock
+	_writeByte(PCF8563_SQW_REG, 1, _data);		// Write the updated square wave register
+	return true;								// Clock enabled successfully
 }
 
 void PCF8563_Class::disableCLK() {
-	_data[0] = 0x00;
-	_writeByte(PCF8563_SQW_REG, 1, _data);
+	_data[0] = 0x00;						// Disable the clock
+	_writeByte(PCF8563_SQW_REG, 1, _data);	// Write the updated square wave register
 }
 
 const char *PCF8563_Class::formatDateTime(uint8_t style) {
-	RTC_Date t = getDateTime();
+	RTC_Date t = getDateTime();	 // Get the current date and time
 	switch (style) {
 		case PCF_TIMEFORMAT_HM:
 			snprintf(format, sizeof(format), "%d:%d", t.hour, t.minute);
@@ -284,6 +284,7 @@ const char *PCF8563_Class::formatDateTime(uint8_t style) {
 }
 
 #ifdef ESP32
+
 bool PCF8563_Class::syncToSystem() {
 	if (PCF8563_Class::isValid()) {
 		struct tm t_tm;
@@ -296,7 +297,7 @@ bool PCF8563_Class::syncToSystem() {
 		t_tm.tm_mon = dt.month - 1;		// Month (starting from January, 0 for January) - Value range is [0,11]
 		t_tm.tm_mday = dt.day;
 
-		val.tv_sec = mktime(&t_tm) - _timezone;	 // make epoch from UTC/GMT even when system timezone already set https://stackoverflow.com/questions/530519/stdmktime-and-timezone-info
+		val.tv_sec = mktime(&t_tm) - _timezone;	 // make epoch from UTC/GMT even when system timezone already set
 		val.tv_usec = 0;
 
 		settimeofday(&val, NULL);  // set system epoch
@@ -320,17 +321,13 @@ bool PCF8563_Class::syncToRtc() {
 		return false;
 	}
 }
+
 #endif
 
 RTC_Date::RTC_Date() : year(0), month(0), day(0), hour(0), minute(0), second(0) {
 }
 
-RTC_Date::RTC_Date(uint16_t y,
-				   uint8_t m,
-				   uint8_t d,
-				   uint8_t h,
-				   uint8_t mm,
-				   uint8_t s) : year(y), month(m), day(d), hour(h), minute(mm), second(s) {
+RTC_Date::RTC_Date(uint16_t y, uint8_t m, uint8_t d, uint8_t h, uint8_t mm, uint8_t s) : year(y), month(m), day(d), hour(h), minute(mm), second(s) {
 }
 
 uint8_t RTC_Date::StringToUint8(const char *pString) {
@@ -350,11 +347,7 @@ uint8_t RTC_Date::StringToUint8(const char *pString) {
 	return value;
 }
 
-RTC_Alarm::RTC_Alarm(
-	uint8_t m,
-	uint8_t h,
-	uint8_t d,
-	uint8_t w) : minute(m), hour(h), day(d), weekday(w) {
+RTC_Alarm::RTC_Alarm(uint8_t m, uint8_t h, uint8_t d, uint8_t w) : minute(m), hour(h), day(d), weekday(w) {
 }
 
 uint8_t PCF8563_Class::status2() {
